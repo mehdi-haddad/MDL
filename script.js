f0450cf28e0cd69851837333ccbb3d9f871d878c
mehdi-haddad <mohamed.mehdi.pasha@gmail.com>
2022-03-29 15:23:09 +0200

    4th

--- script.js
+++ script.js
@@ -3,28 +3,26 @@ function signup() {
     password = document.getElementById('password').value;
     email = document.getElementById('email').value;
     sendData(password, email);
-  }
+}
 
-  function sendData(password, email) {
+function sendData(password, email) {
     var XHR = new XMLHttpRequest();
     var params = 'email='+email+'&password='+password;
 
-
-    
-    // Définissez ce qui se passe si la soumission s'est opérée avec succès
+    // Ce qui se passe si la soumission s'est opérée avec succès
     XHR.addEventListener('load', function(event) {
       alert('Ouais ! Données envoyées et réponse chargée.');
     });
   
-    // Definissez ce qui se passe en cas d'erreur
+    // Ce qui se passe en cas d'erreur
     XHR.addEventListener('error', function(event) {
       alert('Oups! Quelque chose s\'est mal passé.');
     });
   
-    // Configurez la requête
+    // Configuration de la requête
     XHR.open('POST', 'http://localhost:3000/add_user');
     XHR.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
     // Expédiez l'objet FormData ; les en-têtes HTTP sont automatiquement définies
     XHR.send(params);
-  }
+}
